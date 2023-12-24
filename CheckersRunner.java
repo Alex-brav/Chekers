e62b372f444b81552cc56358b56a022abdf89c73
Alex-brav <96271571+Alex-brav@users.noreply.github.com>
2023-12-23 22:18:57 -0600

    Create files

---
+++ CheckersRunner.java
@@ -0,0 +1,5 @@
+public class CheckersRunner{
+    public static void main(String[] args){
+        new Graphics();
+    }
+}
---
+++ Graphics.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.awt.*;
+import java.swing.*;
+
+public class Graphics extends jFrame{
+    Graphics(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+}
